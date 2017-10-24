1ff94d0aa2486160d1e16be1101ed9425385a99e
Adryel Klever <adryelklever3m@hotmail.com>
2017-10-23 23:49:02 -0200

    Metódos adcionados!

--- CalculoDePapel/src/CalculoPapelLinear.java
+++ CalculoDePapel/src/CalculoPapelLinear.java
@@ -1,8 +1,48 @@
 
 public class CalculoPapelLinear {
-    float altura;
-    float alturaMedia = 3;
-    float montagemLateal;
-    float etiquetasUnidades; 
+    private float altura;
+    private float alturaMedia = 3;
+    private float montagemLateal;
+    private float etiquetasUnidades; 
+
+    
+    public float getAltura() {
+        return altura;
+    }
+
+   
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    
+    public float getAlturaMedia() {
+        return alturaMedia;
+    }
+
+    
+    public void setAlturaMedia(float alturaMedia) {
+        this.alturaMedia = alturaMedia;
+    }
+
+   
+    public float getMontagemLateal() {
+        return montagemLateal;
+    }
+
+    
+    public void setMontagemLateal(float montagemLateal) {
+        this.montagemLateal = montagemLateal;
+    }
+
+    
+    public float getEtiquetasUnidades() {
+        return etiquetasUnidades;
+    }
+
+    
+    public void setEtiquetasUnidades(float etiquetasUnidades) {
+        this.etiquetasUnidades = etiquetasUnidades;
+    }
     
 }
